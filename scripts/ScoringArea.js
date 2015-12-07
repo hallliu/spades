60b371a4cbe0c8a906a7079830330da23eaddd27
Hall Liu <hallliu000@gmail.com>
2015-12-06 21:55:34 -0800

    start designing scoring area

---
+++ scripts/ScoringArea.js
@@ -0,0 +1,19 @@
+"use strict";
+
+define(["./Constants", "underscore"], function(Constants, _) {
+    var ScoreEntry = function() {
+        var ScoreEntry = function() {
+
+        };
+    }();
+
+    var ScoringArea = function () {
+        /**
+         * @constructor
+         */
+        var ScoringArea = function() {
+            this.el = $("#scoring-area");
+        };
+
+    }();
+});
